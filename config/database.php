47bae46c7c3c06af7dfe73646ca6ffa1d2946aa5
Hermawan Hantanto <127001588+hermawanhantanto@users.noreply.github.com>
2023-12-19 22:02:11 +0700

    DATABASE ADD CONFIG

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('puri-medika-server.mysql.database.azure.com', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('puri-medika-database', 'forge'),
+            'username' => env('umypdmpobn', 'forge'),
+            'password' => env('88EL45I516UBPXW8$', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQLI_CLIENT_SSL'),
             ]) : [],
         ],
 
